d83be40e2f5ff9fb852e9e9674c205ea333ab4c6
Shashi Liyanage <liyanagemadushani21@gmail.com>
2024-02-29 08:40:59 -0500

    More performant network calls

--- NASAImageLibrary/ViewModel/ImageCollectionViewModel.swift
+++ NASAImageLibrary/ViewModel/ImageCollectionViewModel.swift
@@ -23,65 +23,87 @@ class ImageCollectionViewModel {
         self.apiClient = apiClient
     }
 
+    private var currentTask: Task<Void, Error>?
+
     func loadMediaItems(_ query: String) {
-        Task {
+        // if we are loading the items from a query, we can cancel the current running
+        // task because it's a new search
+        currentTask?.cancel()
+        currentTask = Task {
             do {
                 let mediaCollection = try await apiClient.fetchMediaCollection(for: query)
                 let collectionItems = mediaCollection.items
-                // find the url for the next page
-                let link: Link? = mediaCollection.links.first(where: { link in
-                    link.rel == "next"
-                })
-                nextPage = link?.href
+                setNextPage(for: mediaCollection)
                 // set the items if it's a new query
                 self.items = await loadMediaItems(collectionItems)
+                // set current task to nil if we have the media items
+                currentTask = nil
                 delegate?.didUpdate()
             } catch {
                 // log error
                 NSLog("Error loading media collection for query: \(query), error: \(error)")
+                // cancel the current task and set to nil if there's an error
+                currentTask?.cancel()
+                currentTask = nil
             }
         }
     }
 
     func loadNextPage() {
-        guard let nextPageURLString = nextPage else {
+        // don't load a new page if there's a task running already
+        guard let nextPageURLString = nextPage,
+              currentTask == nil else {
             return
         }
-        Task {
+
+        currentTask = Task {
             do {
                 let mediaCollection = try await apiClient.fetchMediaCollectionNextPage(nextPageURLString)
                 let collectionItems = mediaCollection.items
-                // find the url for the next page
-                let link: Link? = mediaCollection.links.first(where: { link in
-                    link.rel == "next"
-                })
-                nextPage = link?.href
+                setNextPage(for: mediaCollection)
                 // append to current items if we're loading items from next page
                 self.items.append(contentsOf: await loadMediaItems(collectionItems))
+                // set current task to nil if we have the media items
+                currentTask = nil
                 delegate?.didUpdate()
             } catch {
                 // log error
                 NSLog("Error loading next page for url: \(nextPageURLString), error: \(error)")
+                // cancel the current task and set to nil if there's an error
+                currentTask?.cancel()
+                currentTask = nil
             }
         }
     }
 
     private func loadMediaItems(_ collectionItems: [MediaItem]) async -> [MediaDetailViewModel] {
-        let items = await withTaskGroup(of: Void.self, returning: [MediaDetailViewModel].self) { group in
-            collectionItems.compactMap { item in
-                // item data comes in an array, so pick the first item
-                // only display "image" types
-                if !item.data.isEmpty,
-                   let mediaData = item.data.first, mediaData.mediaType == "image" {
-                    let mediaDetailVM = MediaDetailViewModel(apiClient: apiClient, mediaData: mediaData)
-                    group.addTask {
-                        try? await mediaDetailVM.loadMediaDetail()
-                    }
-                    return mediaDetailVM
+        let items = collectionItems.compactMap { item in
+            // item data comes in an array, so pick the first item
+            // only display "image" types
+            if !item.data.isEmpty,
+               let mediaData = item.data.first, mediaData.mediaType == "image" {
+                let mediaDetailVM = MediaDetailViewModel(apiClient: apiClient, mediaData: mediaData)
+                Task {
+                    try? await mediaDetailVM.loadMediaDetail()
                 }
-                return nil
+                return mediaDetailVM
             }
+            return nil
         }
         return items
+    }
+
+    private func setNextPage(for mediaCollection: MediaCollection) {
+        // find the url for the next page
+        let link: Link? = mediaCollection.links.first(where: { link in
+            link.rel == "next"
+        })
+        guard let currentNextPage = link?.href,
+              nextPage != currentNextPage else {
+            // set next page to nil if it's the same as previous next page
+            nextPage = nil
+            return
+        }
+        nextPage = link?.href
     }
 }
--- README.md
+++ README.md
@@ -3,8 +3,6 @@
 ### If I had more time I'll
 - add tests for data decoding, view models
 - search by location and photographer
-- fix the scrolling lag when loading next page
-- better performant network calls
 - add more UI refinements
 - add better error handling - ex. alert controller, errors to reflex http response codes, localized desciptions
 
